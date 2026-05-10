8644e00cc11ea7cd1687862046b7760d4f2fe7d4
Fatih Bilgin <bilginfatih311@gmail.com>
2026-05-10 20:33:48 +0300

    feat(user-service): add redis dependencies and basic configuration

--- user-service/src/main/java/com/logistics/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/logistics/user_service/UserServiceApplication.java
@@ -2,8 +2,11 @@ package com.logistics.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
+
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
